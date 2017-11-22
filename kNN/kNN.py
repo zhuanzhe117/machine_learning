8b730c7814bb6c70549bce570c44c6977c7567ef
gu.haiyan <gu.haiyan@jyall.com>
2017-11-22 18:05:30 +0800

    第一章:k近邻算法：kNN代码,手写识别系统

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -3,6 +3,7 @@ from numpy import *
 import operator
 import matplotlib
 import matplotlib.pyplot as plt
+from os import listdir
 
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -78,6 +79,42 @@ def classifyPerson():
     inArr = array([ffMiles,percentTats,iceCream])
     classifierResult = classify0((inArr-minVals)/ranges,norMat,datingLabels,3)
     print "You will probably like this person: ",resultList[classifierResult - 1]
+
+#将图像转换为测试向量
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+#手写数字识别系统的测试代码
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print "the classifier came back with : %d, the real answer is : %d" % (classifierResult,classNumStr)
+        if(classifierResult != classNumStr): errorCount += 1.0
+        print "\nthe total number of errors is : %d" % errorCount
+        print "\nthe total error rate is : %f" % (errorCount/float(mTest))
 
 if __name__ == '__main__':
     #样本训练集
