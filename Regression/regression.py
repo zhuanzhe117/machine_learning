eae0683cd9ef0c899c31d803499e2f6e6dd1f442
gu.haiyan <gu.haiyan@jyall.com>
2018-01-22 19:08:45 +0800

    第八章:回归

--- Regression/regression.py
+++ Regression/regression.py
@@ -25,6 +25,28 @@ def standRegres(xArr,yArr):
     #ws = linalg.solve(xTx,xMat.T*yMat)  使用函数求解未知矩阵
     return ws
 
+#局部加权线性回归函数
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat = mat(xArr);yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))#创建对角矩阵
+    for j in range(m):
+        diffMat = testPoint - xMat[j,:] #权重值大小以指数级衰减
+        weights[j,j] = exp(diffMat * diffMat.T/(-2.0 * k ** 2))
+    xTx = xMat.T * (weights*xMat)
+    if linalg.det(xTx) == 0.0: #奇异矩阵
+        print 'This matrix is singular, cannot do inverse'
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
 if __name__ == '__main__':
     xArr ,yArr = loadDataSet('data/ex0.txt')
     weights = standRegres(xArr,yArr)
@@ -36,7 +58,7 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
 
-    xCopy = xMat.copy() #
+    xCopy = xMat.copy() #将直线上的数据点按照升序排列？
     xCopy.sort(0)
     yHat = xCopy * weights #预测值
     ax.plot(xCopy[:,1],yHat)
