6240e77c3e0ba764edbdb72f80019ab7cecb5f67
gu.haiyan <gu.haiyan@jyall.com>
2018-01-23 17:42:05 +0800

    第八章:回归

--- Regression/regression.py
+++ Regression/regression.py
@@ -26,13 +26,14 @@ def standRegres(xArr,yArr):
     return ws
 
 #局部加权线性回归函数
+#先求公式中的W（待测点附近的每个点的权重），即weights，然后求回归系数ws，返回待测点的y值
 def lwlr(testPoint,xArr,yArr,k=1.0):
     xMat = mat(xArr);yMat = mat(yArr).T
     m = shape(xMat)[0]
     weights = mat(eye((m)))#创建对角矩阵
     for j in range(m):
         diffMat = testPoint - xMat[j,:] #权重值大小以指数级衰减
-        weights[j,j] = exp(diffMat * diffMat.T/(-2.0 * k ** 2))
+        weights[j,j] = exp(diffMat * diffMat.T/(-2.0 * k ** 2)) #运用高斯核对附近点赋予权重，点越近权重越大
     xTx = xMat.T * (weights*xMat)
     if linalg.det(xTx) == 0.0: #奇异矩阵
         print 'This matrix is singular, cannot do inverse'
@@ -40,6 +41,7 @@ def lwlr(testPoint,xArr,yArr,k=1.0):
     ws = xTx.I * (xMat.T * (weights * yMat))
     return testPoint * ws
 
+#测试局部加权函数，传入多个待测点
 def lwlrTest(testArr,xArr,yArr,k=1.0):
     m = shape(testArr)[0]
     yHat = zeros(m)
@@ -49,18 +51,32 @@ def lwlrTest(testArr,xArr,yArr,k=1.0):
 
 if __name__ == '__main__':
     xArr ,yArr = loadDataSet('data/ex0.txt')
-    weights = standRegres(xArr,yArr)
-    import matplotlib.pyplot as plt
-    xMat = mat(xArr)
-    yMat = mat(yArr) #真实值
+    # weights = standRegres(xArr,yArr)
+    # import matplotlib.pyplot as plt
+    # #画样本点
+    # xMat = mat(xArr)
+    # yMat = mat(yArr) #真实值
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
+    #
+    # # 画预测点，将直线上的数据点按照升序排列
+    # xCopy = xMat.copy()
+    # xCopy.sort(0)
+    # yHat = xCopy * weights #预测值
+    # ax.plot(xCopy[:,1],yHat)
+    # plt.show()
 
+    #局部加权效果
+    yHat = lwlrTest(xArr,xArr,yArr,0.01)
+    xMat = mat(xArr)
+    srtInd = xMat[:,1].argsort(0)
+    xSort = xMat[srtInd][:,0,:]
+
+    import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
-
-    xCopy = xMat.copy() #将直线上的数据点按照升序排列？
-    xCopy.sort(0)
-    yHat = xCopy * weights #预测值
-    ax.plot(xCopy[:,1],yHat)
-
+    ax.plot(xSort[:,1],yHat[srtInd])
+    ax.scatter(xMat[:,1].flatten().A[0],mat(yArr).T.flatten().A[0],s=2,c='red')
     plt.show()
+
