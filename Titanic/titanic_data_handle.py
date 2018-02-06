0c5b0aa8e248deec55bc70d37fe94058a8e91e86
gu.haiyan <gu.haiyan@jyall.com>
2018-02-06 17:28:48 +0800

    项目：Titanic

---
+++ Titanic/titanic_data_handle.py
@@ -0,0 +1,65 @@
+#encoding=utf-8
+'''
+kaggle案例：泰坦尼克号之灾，预测谁能生还： 数据处理
+'''
+
+import pandas as pd #数据分析
+import numpy as np #科学计算
+
+data_train = pd.read_csv("data/Train.csv")
+
+#用scikit-learn中的RandomForest来拟合一下缺失的年龄数据
+from sklearn.ensemble import RandomForestRegressor
+def set_missing_ages(df):
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+    # X即特征属性值
+    X = known_age[:, 1:]
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+    # 用得到的预测结果填补原缺失数据
+    df.loc[(df.Age.isnull()), 'Age'] = predictedAges
+    return df, rfr
+
+def set_Cabin_type(df):
+    df.loc[(df.Cabin.notnull()), 'Cabin'] = "Yes"
+    df.loc[(df.Cabin.isnull()), 'Cabin'] = "No"
+    return df
+
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+
+# 因为逻辑回归建模时，需要输入的特征都是数值型特征
+# 我们先对类目型的特征离散/因子化
+# 以Cabin为例，原本一个属性维度，因为其取值可以是['yes','no']，而将其平展开为'Cabin_yes','Cabin_no'两个属性
+# 原本Cabin取值为yes的，在此处的'Cabin_yes'下取值为1，在'Cabin_no'下取值为0
+# 原本Cabin取值为no的，在此处的'Cabin_yes'下取值为0，在'Cabin_no'下取值为1
+# 我们使用pandas的get_dummies来完成这个工作，并拼接在原来的data_train之上
+
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+
+# 接下来我们要接着做一些数据预处理的工作，比如scaling，将一些变化幅度较大的特征化到[-1,1]之内
+# 这样可以加速logistic regression的收敛
+import sklearn.preprocessing as preprocessing
+scaler = preprocessing.StandardScaler()
+age_scale_param = scaler.fit(df['Age'])
+df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
+fare_scale_param = scaler.fit(df['Fare'])
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
+df
