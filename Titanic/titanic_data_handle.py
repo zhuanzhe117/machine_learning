4ba638827e9d9956d94daa058ec3d3191e6bc2a9
gu.haiyan <gu.haiyan@jyall.com>
2018-02-07 14:25:47 +0800

    项目：Titanic

--- Titanic/titanic_data_handle.py
+++ Titanic/titanic_data_handle.py
@@ -7,13 +7,12 @@ import pandas as pd #数据分析
 import numpy as np #科学计算
 
 data_train = pd.read_csv("data/Train.csv")
-
-#用scikit-learn中的RandomForest来拟合一下缺失的年龄数据
+#*************************************处理数据***************************
+#用scikit-learn中的RandomForest拟合缺失的年龄数据
 from sklearn.ensemble import RandomForestRegressor
 def set_missing_ages(df):
     # 把已有的数值型特征取出来丢进Random Forest Regressor中
     age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
-
     # 乘客分成已知年龄和未知年龄两部分
     known_age = age_df[age_df.Age.notnull()].as_matrix()
     unknown_age = age_df[age_df.Age.isnull()].as_matrix()
@@ -54,12 +53,59 @@ dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
 df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
 df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
 
-# 接下来我们要接着做一些数据预处理的工作，比如scaling，将一些变化幅度较大的特征化到[-1,1]之内
+# 接下来做一些数据预处理的工作，比如scaling，将一些变化幅度较大的特征化到[-1,1]之内
 # 这样可以加速logistic regression的收敛
 import sklearn.preprocessing as preprocessing
 scaler = preprocessing.StandardScaler()
-age_scale_param = scaler.fit(df['Age'])
-df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
-fare_scale_param = scaler.fit(df['Fare'])
-df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
-df
+# age_scale_param = scaler.fit(df['Age'])
+# df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
+df['Age_scaled'] = scaler.fit_transform(df['Age'].values.reshape(-1,1))
+# fare_scale_param = scaler.fit(df['Fare'])
+# df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'].values.reshape(-1,1))
+print df
+
+#***********************************训练算法*******************************
+# 我们把需要的feature字段取出来，转成numpy格式，使用scikit-learn中的LogisticRegression建模
+from sklearn import linear_model
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+# X即特征属性值
+X = train_np[:, 1:]
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(X, y)
+clf
+
+#***********************************测试算法******************************
+data_test = pd.read_csv("data/test.csv")
+data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
+# 接着我们对test_data做和train_data中一致的特征变换
+# 首先用同样的RandomForestRegressor模型填上丢失的年龄
+tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[ (data_test.Age.isnull()), 'Age' ] = predictedAges
+
+data_test = set_Cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix= 'Pclass')
+
+df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'].values.reshape(-1,1))
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'].values.reshape(-1,1))
+df_test
+
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions.astype(np.int32)})
+result.to_csv("data/logistic_regression_predictions.csv", index=False)
+pd.read_csv("data/logistic_regression_predictions.csv")
