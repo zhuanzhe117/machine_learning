e7c3ef0fc8bdc1319222941f137b3d0b212491b9
Hannah <zhuanzhe117@163.com>
2018-08-13 15:52:45 +0800

    word2vec-nlp-bag of words总结

--- word2vec-nlp/bag_of_words.py
+++ word2vec-nlp/bag_of_words.py
@@ -1,41 +1,49 @@
 # _*_ coding: utf-8 _*_
 
 import pandas as pd
-train = pd.read_csv("D:/materials/dataset/word2vec-nlp/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
-print train.shape
-print train.columns.values
-
 from bs4 import BeautifulSoup
-
-# Initialize the BeautifulSoup object on a single movie review
-example1 = BeautifulSoup(train["review"][0],"html.parser")
-print train["review"][0]
-print example1.get_text()
-
 import re
-# Use regular expressions to do a find-and-replace
-letters_only = re.sub("[^a-zA-Z]",           # The pattern to search for
-                      " ",                   # The pattern to replace it with
-                      example1.get_text() )  # The text to search
-print letters_only
-lower_case = letters_only.lower()        # Convert to lower case
-words = lower_case.split()               # Split into words
-
 import nltk
-nltk.download()
-
 from nltk.corpus import stopwords
-print stopwords.words("english")
-# Remove stop words from "words"
-words = [w for w in words if not w in stopwords.words("english")]
-print words
+
+def preprocess_data(train):
+    num_reviews = train["review"].size
+    print "Cleaning and parsing the training set movie reviews...\n"
+    clean_train_reviews = []
+
+    # Loop over each review; create an index i that goes from 0 to the length
+    # of the movie review list
+    for i in range(0, num_reviews):
+        if ((i + 1) % 1000 == 0):
+            print "Review %d of %d\n" % (i + 1, num_reviews)
+        clean_train_reviews.append(review_to_words(train["review"][i]))
+
+    print "Creating the bag of words...\n"
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    # Initialize the "CountVectorizer" object, which is scikit-learn's
+    # bag of words tool.
+    vectorizer = CountVectorizer(analyzer="word",
+                                 tokenizer=None,
+                                 preprocessor=None,
+                                 stop_words=None,
+                                 max_features=5000)
+
+    # fit_transform() does two functions: First, it fits the model
+    # and learns the vocabulary; second, it transforms our training data
+    # into feature vectors. The input to fit_transform should be a list of strings.
+    train_data_features = vectorizer.fit_transform(clean_train_reviews)
+
+    # Numpy arrays are easy to work with, so convert the result to an array
+    train_data_features = train_data_features.toarray()
+    return train_data_features,vectorizer
 
 def review_to_words( raw_review ):
     # Function to convert a raw review to a string of words
     # The input is a single string (a raw movie review), and
     # the output is a single string (a preprocessed movie review)
     # 1. Remove HTML
-    review_text = BeautifulSoup(raw_review).get_text()
+    review_text = BeautifulSoup(raw_review,"html.parser").get_text()
     #
     # 2. Remove non-letters
     letters_only = re.sub("[^a-zA-Z]", " ", review_text)
@@ -54,76 +62,70 @@ def review_to_words( raw_review ):
     # and return the result.
     return( " ".join( meaningful_words ))
 
-num_reviews = train["review"].size
-print "Cleaning and parsing the training set movie reviews...\n"
-clean_train_reviews = []
+def train_model_classify(train_data_features,vectorizer):
+    print "Training the random forest..."
+    from sklearn.ensemble import RandomForestClassifier
 
-# Loop over each review; create an index i that goes from 0 to the length
-# of the movie review list
-for i in range( 0, num_reviews ):
-    if ((i + 1) % 1000 == 0):
-        print "Review %d of %d\n" % (i + 1, num_reviews)
-    clean_train_reviews.append( review_to_words( train["review"][i] ) )
+    # Initialize a Random Forest classifier with 100 trees
+    forest = RandomForestClassifier(n_estimators=100)
 
-print "Creating the bag of words...\n"
-from sklearn.feature_extraction.text import CountVectorizer
+    # Fit the forest to the training set, using the bag of words as
+    # features and the sentiment labels as the response variable
+    #
+    # This may take a few minutes to run
+    forest = forest.fit(train_data_features, train["sentiment"])
 
-# Initialize the "CountVectorizer" object, which is scikit-learn's
-# bag of words tool.
-vectorizer = CountVectorizer(analyzer = "word",
-                             tokenizer = None,
-                             preprocessor = None,
-                             stop_words = None,
-                             max_features = 5000)
+    # Read the test data
+    test = pd.read_csv("D:/materials/dataset/word2vec-nlp/testData.tsv", header=0, delimiter="\t", quoting=3)
+    print test.shape
+    # Create an empty list and append the clean reviews one by one
+    num_reviews = len(test["review"])
+    clean_test_reviews = []
 
-# fit_transform() does two functions: First, it fits the model
-# and learns the vocabulary; second, it transforms our training data
-# into feature vectors. The input to fit_transform should be a list of strings.
-train_data_features = vectorizer.fit_transform(clean_train_reviews)
+    print "Cleaning and parsing the test set movie reviews...\n"
+    for i in xrange(0, num_reviews):
+        if ((i + 1) % 1000 == 0):
+            print "Review %d of %d\n" % (i + 1, num_reviews)
+        clean_review = review_to_words(test["review"][i])
+        clean_test_reviews.append(clean_review)
 
-# Numpy arrays are easy to work with, so convert the result to an array
-train_data_features = train_data_features.toarray()
+    # Get a bag of words for the test set, and convert to a numpy array
+    test_data_features = vectorizer.transform(clean_test_reviews)
+    test_data_features = test_data_features.toarray()
 
-print "Training the random forest..."
-from sklearn.ensemble import RandomForestClassifier
+    # Use the random forest to make sentiment label predictions
+    result = forest.predict(test_data_features)
 
-# Initialize a Random Forest classifier with 100 trees
-forest = RandomForestClassifier(n_estimators = 100)
+    # Copy the results to a pandas dataframe with an "id" column and
+    # a "sentiment" column
+    output = pd.DataFrame(data={"id": test["id"], "sentiment": result})
 
-# Fit the forest to the training set, using the bag of words as
-# features and the sentiment labels as the response variable
-#
-# This may take a few minutes to run
-forest = forest.fit( train_data_features, train["sentiment"] )
+    # Use pandas to write the comma-separated output file
+    output.to_csv("D:/materials/dataset/word2vec-nlp/Bag_of_Words_model.csv", index=False, quoting=3)
 
-# Read the test data
-test = pd.read_csv("testData.tsv", header=0, delimiter="\t", \
-                   quoting=3 )
+if __name__=="__main__":
+    train = pd.read_csv("D:/materials/dataset/word2vec-nlp/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+    print train.shape
+    print train.columns.values
+    train_data_features, vectorizer = preprocess_data(train)
+    train_model_classify(train_data_features, vectorizer)
+    # from bs4 import BeautifulSoup
+    # example1 = BeautifulSoup(train["review"][0], "html.parser")
+    # print train["review"][0]
+    # print example1.get_text()
+    #
+    # import re
+    # letters_only = re.sub("[^a-zA-Z]",  # The pattern to search for
+    #                       " ",  # The pattern to replace it with
+    #                       example1.get_text())  # The text to search
+    # print letters_only
+    # lower_case = letters_only.lower()  # Convert to lower case
+    # words = lower_case.split()  # Split into words
+    #
+    # import nltk
+    # # nltk.download()
+    # from nltk.corpus import stopwords
+    # print stopwords.words("english")
+    # words = [w for w in words if not w in stopwords.words("english")]
+    # print words
 
-# Verify that there are 25,000 rows and 2 columns
-print test.shape
-
-# Create an empty list and append the clean reviews one by one
-num_reviews = len(test["review"])
-clean_test_reviews = []
-
-print "Cleaning and parsing the test set movie reviews...\n"
-for i in xrange(0,num_reviews):
-    if( (i+1) % 1000 == 0 ):
-        print "Review %d of %d\n" % (i+1, num_reviews)
-    clean_review = review_to_words( test["review"][i] )
-    clean_test_reviews.append( clean_review )
-
-# Get a bag of words for the test set, and convert to a numpy array
-test_data_features = vectorizer.transform(clean_test_reviews)
-test_data_features = test_data_features.toarray()
-
-# Use the random forest to make sentiment label predictions
-result = forest.predict(test_data_features)
-
-# Copy the results to a pandas dataframe with an "id" column and
-# a "sentiment" column
-output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
-
-# Use pandas to write the comma-separated output file
-output.to_csv( "Bag_of_Words_model.csv", index=False, quoting=3 )
