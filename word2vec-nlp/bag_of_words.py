26b87d7d06c82525c619871098dccd9016146df1
Hannah <zhuanzhe117@163.com>
2018-10-26 21:39:43 +0800

    word2vec词袋模型训练

--- word2vec-nlp/bag_of_words.py
+++ word2vec-nlp/bag_of_words.py
@@ -4,106 +4,78 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
 def review_to_words( raw_review ):
-    # Function to convert a raw review to a string of words
-    # The input is a single string (a raw movie review), and
-    # the output is a single string (a preprocessed movie review)
+
     # 1. Remove HTML
     review_text = BeautifulSoup(raw_review,"html.parser").get_text()
-    #
+
     # 2. Remove non-letters
     letters_only = re.sub("[^a-zA-Z]", " ", review_text)
-    #
+
     # 3. Convert to lower case, split into individual words
     words = letters_only.lower().split()
-    #
-    # 4. In Python, searching a set is much faster than searching
-    #   a list, so convert the stop words to a set
+
+    # 4.在Python中查找set比list快
     stops = set(stopwords.words("english"))
-    #
+
     # 5. Remove stop words
     meaningful_words = [w for w in words if not w in stops]
-    #
-    # 6. Join the words back into one string separated by space,
-    # and return the result.
+
     return( " ".join( meaningful_words ))
 
 def preprocess_data(train):
     num_reviews = train["review"].size
-    print "Cleaning and parsing the training set movie reviews...\n"
+    print "开始清理和分析训练集的影评...\n"
     clean_train_reviews = []
 
-    # Loop over each review; create an index i that goes from 0 to the length
-    # of the movie review list
     for i in range(0, num_reviews):
         if ((i + 1) % 1000 == 0):
             print "Review %d of %d\n" % (i + 1, num_reviews)
         clean_train_reviews.append(review_to_words(train["review"][i]))
 
     print "Creating the bag of words...\n"
-    from sklearn.feature_extraction.text import CountVectorizer
-
-    # Initialize the "CountVectorizer" object, which is scikit-learn's
-    # bag of words tool.
-    vectorizer = CountVectorizer(analyzer="word",
-                                 tokenizer=None,
-                                 preprocessor=None,
-                                 stop_words=None,
+    # 初始化CountVectorizer对象, 是scikit-learn的词袋模型工具
+    vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None,
                                  max_features=5000)
 
-    # fit_transform() does two functions: First, it fits the model
-    # and learns the vocabulary; second, it transforms our training data
-    # into feature vectors. The input to fit_transform should be a list of strings.
+    # fit_transform() : First, it fits the model and learns the vocabulary;
+    #  second, it transforms our training data into feature vectors. The input to fit_transform should be a list of strings.
     train_data_features = vectorizer.fit_transform(clean_train_reviews)
 
-    # Numpy arrays are easy to work with, so convert the result to an array
     train_data_features = train_data_features.toarray()
     return train_data_features,vectorizer
 
 def train_model_classify(train_data_features,vectorizer):
     print "Training the random forest..."
-    from sklearn.ensemble import RandomForestClassifier
 
-    # Initialize a Random Forest classifier with 100 trees
     forest = RandomForestClassifier(n_estimators=100)
-
-    # Fit the forest to the training set, using the bag of words as
-    # features and the sentiment labels as the response variable
-    #
-    # This may take a few minutes to run
     forest = forest.fit(train_data_features, train["sentiment"])
 
-    # Read the test data
-    test = pd.read_csv("D:/materials/dataset/word2vec-nlp/testData.tsv", header=0, delimiter="\t", quoting=3)
+    test = pd.read_csv("data/testData.tsv", header=0, delimiter="\t", quoting=3)
     print test.shape
-    # Create an empty list and append the clean reviews one by one
     num_reviews = len(test["review"])
     clean_test_reviews = []
 
-    print "Cleaning and parsing the test set movie reviews...\n"
+    print "清理和分析测试集的影评...\n"
     for i in xrange(0, num_reviews):
         if ((i + 1) % 1000 == 0):
             print "Review %d of %d\n" % (i + 1, num_reviews)
         clean_review = review_to_words(test["review"][i])
         clean_test_reviews.append(clean_review)
 
-    # Get a bag of words for the test set, and convert to a numpy array
     test_data_features = vectorizer.transform(clean_test_reviews)
     test_data_features = test_data_features.toarray()
 
-    # Use the random forest to make sentiment label predictions
     result = forest.predict(test_data_features)
-
-    # Copy the results to a pandas dataframe with an "id" column and
-    # a "sentiment" column
-    output = pd.DataFrame(data={"id": test["id"], "sentiment": result})
-
+    df = pd.DataFrame(data={"id": test["id"], "sentiment": result})
     # Use pandas to write the comma-separated output file
-    output.to_csv("D:/materials/dataset/word2vec-nlp/Bag_of_Words_model.csv", index=False, quoting=3)
+    df.to_csv("data/Bag_of_Words_Result.csv", index=False, quoting=3)
 
 if __name__=="__main__":
-    train = pd.read_csv("D:/materials/dataset/word2vec-nlp/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
+    train = pd.read_csv("data/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
     print train.shape
     print train.columns.values
     train_data_features, vectorizer = preprocess_data(train)
--- word2vec-nlp/word_vector.py
+++ word2vec-nlp/word_vector.py
@@ -1,81 +1,73 @@
 # _*_ coding: utf-8 _*_
 
-import pandas as pd
-train = pd.read_csv( "D:/materials/dataset/word2vec-nlp/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3,encoding='utf-8' )
-test = pd.read_csv( "D:/materials/dataset/word2vec-nlp/testData.tsv", header=0, delimiter="\t", quoting=3 ,encoding='utf-8' )
-unlabeled_train = pd.read_csv( "D:/materials/dataset/word2vec-nlp/unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3,encoding='utf-8' )
-
-# Verify the number of reviews that were read (100,000 in total)
-print "Read %d labeled train reviews, %d labeled test reviews, " \
- "and %d unlabeled reviews\n" % (train["review"].size,
- test["review"].size, unlabeled_train["review"].size )
-
-from bs4 import BeautifulSoup
 import re
+
+import pandas as pd
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 
-def review_to_wordlist( review, remove_stopwords=False ):
-    # Function to convert a document to a sequence of words,
-    # optionally removing stop words.  Returns a list of words.
-    #
-    # 1. Remove HTML
-    review_text = BeautifulSoup(review,"html.parser").get_text()
-    #
-    # 2. Remove non-letters
-    review_text = re.sub("[^a-zA-Z]"," ", review_text)
-    #
-    # 3. Convert words to lower case and split them
-    words = review_text.lower().split()
-    # 4. Optionally remove stop words (false by default)
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in stops]
-    # 5. Return a list of words
-    return(words)
+def load_data():
+    train = pd.read_csv("data/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3, encoding='utf-8')
+    test = pd.read_csv("data/testData.tsv", header=0, delimiter="\t", quoting=3, encoding='utf-8')
+    unlabeled_train = pd.read_csv("data/unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3, encoding='utf-8')
+
+    # 影评数量一共100,000条
+    print "Read %d labeled train reviews, %d labeled test reviews, " \
+          "and %d unlabeled reviews\n" % (train["review"].size, test["review"].size, unlabeled_train["review"].size)
+    return train,test,unlabeled_train
 
 # Download the punkt tokenizer for sentence splitting
 import nltk.data
 # nltk.download()
-
 # Load the punkt tokenizer
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-# Define a function to split a review into parsed sentences
 def review_to_sentences( review, tokenizer, remove_stopwords=False ):
-    # Function to split a review into parsed sentences. Returns a
-    # list of sentences, where each sentence is a list of words
+    '''
+    将影评切分成句子，将每句都进行分词
+    :param review: 一条影评
+    :param tokenizer:标记器
+    :param remove_stopwords:是否去除停用词
+    :return:句子列表，每个句子都是一个词列表
+    '''
+
     # print review 运行时建议打开
-    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    # Use the NLTK tokenizer to split the paragraph into sentences
     raw_sentences = tokenizer.tokenize(review.strip())
-    #
-    # 2. Loop over each sentence
     sentences = []
     for raw_sentence in raw_sentences:
-        # If a sentence is empty, skip it
         if len(raw_sentence) > 0:
-            # Otherwise, call review_to_wordlist to get a list of words
             sentences.append( review_to_wordlist( raw_sentence, remove_stopwords ))
-
-    # Return the list of sentences (each sentence is a list of words,
-    # so this returns a list of lists
     return sentences
 
-def train_the_model():
+def review_to_wordlist( review, remove_stopwords=False ):
+    '''
+    将一条影评转换成词列表
+    :param review:
+    :param remove_stopwords:
+    :return:
+    '''
+    review_text = BeautifulSoup(review,"html.parser").get_text()
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    words = review_text.lower().split()
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    return(words)
+
+def train_the_model(train,unlabeled_train):
     sentences = []
-    print "Parsing sentences from training set"
+    print "将有标签的数据集的影评切分成句子，再切分成词..."
     for review in train["review"]:
         sentences += review_to_sentences(review, tokenizer)
 
-    print "Parsing sentences from unlabeled set"
+    print "将无标签的数据集的影评切分成句子，再切分成词..."
     for review in unlabeled_train["review"]:
         sentences += review_to_sentences(review, tokenizer)
 
-    # Import the built-in logging module and configure it so that Word2Vec
-    # creates nice output messages
     import logging
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-    # Set values for various parameters
     num_features = 300  # Word vector dimensionality
     min_word_count = 40  # Minimum word count
     num_workers = 4  # Number of threads to run in parallel
@@ -92,7 +84,9 @@ def train_the_model():
     # init_sims will make the model much more memory-efficient.
     model.init_sims(replace=True)
 
-    # It can be helpful to create a meaningful model name and
-    # save the model for later use. You can load it later using Word2Vec.load()
-    model_name = "300features_40minwords_10context"
+    # 保存模型，可用Word2Vec.load()加载
+    model_name = "300features_40minwords_10context1"
     model.save(model_name)
+
+train,test,unlabeled_train = load_data()
+train_the_model(train,unlabeled_train)
